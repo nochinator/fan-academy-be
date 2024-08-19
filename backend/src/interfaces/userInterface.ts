ac741f714809f98529bb2d1224cee94191617fa5
Dan Diaz <daniel.diaz.hdez@gmail.com>
2024-08-19 21:04:04 +0200

    feat: updated IUser interface

--- backend/src/interfaces/userInterface.ts
+++ backend/src/interfaces/userInterface.ts
@@ -1,4 +1,16 @@
 import { ObjectId } from "mongoose";
+import { EFaction, EWinConditions } from "../enums/game.enums";
+
+interface IArchivedGame {
+  gameId: string;
+  playerFaction: EFaction;
+  oponentId: string;
+  oponentFaction: EFaction;
+  startDate: Date;
+  endDate: Date;
+  won: boolean;
+  winCondition: EWinConditions;
+}
 
 interface IUser {
   _id: ObjectId;
@@ -7,6 +19,8 @@ interface IUser {
   password?: string;
   googleId?: string
   picture?: string;
+  currentGames?: string[];
+  gameHistory: IArchivedGame[]; // currentGames become archived here once finished
 }
 
 export default IUser;
